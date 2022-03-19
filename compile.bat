2e9257a771427c730c83ab71f545610b8b5b47d5
Koekto-code <97475827+Koekto-code@users.noreply.github.com>
2022-03-19 16:16:47 +0200

    Update and rename compile.bat to mingw32-compile.bat

--- compile.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd source
-mingw32-g++ gl.c -c -O2 -I"D:\Libs\glad\include"
-mingw32-g++ main.cpp -c -O2 -I"D:\Libs\stb" -I"D:\Libs\glm-master" -I"D:\Libs\glad\include" -I"D:\Libs\glfw-3.3.4\include"
-pause
-mingw32-g++ gl.o main.o -s -o "../Release/ctest.exe" -lglfw3dll -L"D:\Libs\glfw-3.3.4\mingw32\src" -Wl,-subsystem,windows
-pause
