3b1e08a568c57969aee42583c0b2ce787e429041
Koekto-code <97475827+Koekto-code@users.noreply.github.com>
2022-06-27 17:14:41 +0300

    Delete source directory

--- source/models/sph.h
+++
@@ -1,5 +0,0 @@
-// 27 june 2022
-// The main planet OpenGL model
-
-extern float sphVerts[145805U];
-extern unsigned long sphIndices[171360U];
--- source/runtime/stb_image.c
+++
@@ -1,4 +0,0 @@
-// 27 june 2022
-
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
